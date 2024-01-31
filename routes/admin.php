b94811fa13d94d7b51b8866204a23d07753530a9
Ingo Ruddat <42838916+Ruddat@users.noreply.github.com>
2024-01-31 20:32:57 +0100

    PreventBackhistory added

--- routes/admin.php
+++ routes/admin.php
@@ -6,12 +6,12 @@ use App\Http\Controllers\AdminController;
 
 Route::prefix('admin')->name('admin.')->group(function(){
 
-    Route::middleware(['guest:admin'])->group(function(){
+    Route::middleware(['guest:admin', 'PreventBackHistory'])->group(function(){
         Route::view('/login', 'backend.pages.admin.auth.login')->name('login');
         Route::post('/login_handler', [AdminController::class, 'loginHandler'])->name('login_handler');
     });
 
-    Route::middleware(['auth:admin'])->group(function () {
+    Route::middleware(['auth:admin', 'PreventBackHistory'])->group(function () {
         Route::view('/home', 'backend.pages.admin.home')->name('home');
         Route::post('/logout_handler', [AdminController::class, 'logoutHandler'])->name('logout_handler');
     });
