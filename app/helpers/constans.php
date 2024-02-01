cc90facacf831e3fb65c6ee25468d703c6fafefd
Ingo Ruddat <42838916+Ruddat@users.noreply.github.com>
2024-02-01 04:12:32 +0100

    helpers phpmailer config added

---
+++ app/helpers/constans.php
@@ -0,0 +1,14 @@
+<?php
+
+class constGuards
+{
+    const ADMIN = 'admin';
+    const SELLER = 'seller';
+    const CLIENT = 'client';
+}
+
+class constDefaults
+{
+    const tokenExpiredMinutes = 15;
+   
+}
---
+++ app/helpers/helpers.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+/** SEND EMAIL FUNCTION USING PHPMAILER LIBRARY */
+if( !function_exists('send_mail')){
+
+    function sendEmail($mailConfig){
+
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
+
+    $mail = new PHPMailer(true);
+    $mail->SMTPDebug = 0;                                        // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = env('EMAIL_HOST');                     // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = env('EMAIL_USERNAME');                     // SMTP username
+    $mail->Password   = env('EMAIL_PASSWORD');                               // SMTP password
+    $mail->SMTPSecure = env('EMAIL_ENCRYPTION');         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = env('EMAIL_PORT');                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->setFrom($mailConfig['mail_from_email'], $mailConfig['mail_from_name']);
+    $mail->addAddress($mailConfig['mail_recipient_email'], $mailConfig['mail_recipiemt_name']);     // Add a recipient
+
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $mailConfig['mail_subject'];
+    $mail->Body    = $mailConfig['mail_body'];
+    if ($mail->send()) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+    }
+
+}
