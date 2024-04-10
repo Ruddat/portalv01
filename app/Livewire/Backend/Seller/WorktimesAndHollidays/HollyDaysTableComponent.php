d09b212ea5380d7c931e13a98c79f8d8654a5006
Ingo Ruddat <42838916+Ruddat@users.noreply.github.com>
2024-04-10 07:09:52 +0200

    Hollydays Table added

---
+++ app/Livewire/Backend/Seller/WorktimesAndHollidays/HollyDaysTableComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Backend\Seller\WorktimesAndHollidays;
+
+use Livewire\Component;
+
+class HollyDaysTableComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.backend.seller.worktimes-and-hollidays.holly-days-table-component');
+    }
+}
---
+++ resources/views/livewire/backend/seller/worktimes-and-hollidays/holly-days-table-component.blade.php
@@ -0,0 +1,93 @@
+<div>
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Custom Openinghours</h4>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table header-border table-hover verticle-middle">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">from</th>
+                                <th scope="col">to</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <tr>
+                                <th>Holiday/Custom</th>
+                                    <td>
+                                        <div class="mb-3 col-md-6">
+                                            <input type="date" class="form-control" placeholder="1234 Main St" spellcheck="false" data-ms-editor="true">
+                                        </div>
+                                        </td>
+                                    <td>
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input center" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                                        </div>
+                                    </td>
+                                        <td>
+                                                <input type="time" class="form-control" placeholder="Email">
+                                        </td>
+
+                                        <td>
+                                                <input type="time" class="form-control" placeholder="Email">
+                                        </td>
+
+                                        <td>
+                                            <button type="submit" class="btn btn-success">Add</button>
+
+                                        </td>
+
+
+                            </tr>
+
+                            <tr>
+                                <th>Holiday/Custom</th>
+
+
+                                    <td>
+                                        <div class="mb-3 col-md-6">
+                                            <input type="date" class="form-control" placeholder="1234 Main St" spellcheck="false" data-ms-editor="true">
+                                        </div>
+                                        </td>
+
+                                    <td>
+
+                                        <div class="form-check form-switch mb-3 col-md-2">
+                                            <input class="form-check-input center" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                                        </div>
+                                    </td>
+
+
+                                        <td>
+                                                <input type="time" class="form-control" placeholder="Email">
+                                        </td>
+
+                                        <td>
+                                                <input type="time" class="form-control" placeholder="Email">
+                                        </td>
+
+                                        <td>
+                                                <button type="submit" class="btn btn-secondary">Remove</button>
+
+                                        </td>
+
+
+                            </tr>
+
+
+
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
